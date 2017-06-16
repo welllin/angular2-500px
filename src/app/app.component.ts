2a13d18c076a82d087c0cac7830d937726ff9fbe
Well <Well@Wells-MacBook-Air.local>
2017-06-16 20:17:23 +1200

    Added type

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
+
 import { ApiService } from 'app/core/services/api.service';
+import { IData } from 'app/core/types/Data';
+
 declare var $: any;
 
 @Component({
@@ -8,13 +12,19 @@ declare var $: any;
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  private data: IData[];
+
   constructor(
     private apiService: ApiService
   ) { }
 
   ngOnInit() {
-    this.apiService.search('cat').subscribe((data) => {
-      console.log(data);
-    })
+
+  }
+
+  search(term: string): any {
+    this.apiService.search(term).subscribe((data) => {
+      return data;
+    });
   }
 }
---
+++ src/app/core/types/Data.ts
@@ -0,0 +1,6 @@
+export interface IData {
+    current_page: number;
+    photos: [any];
+    total_items: number;
+    total_pages: number;
+}
