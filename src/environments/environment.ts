b5f92e468ff062fb5a35e746c81ce72c13b2fa7b
Well <Well@Wells-MacBook-Air.local>
2017-06-16 19:28:51 +1200

    Integrated with jQuery & Semantic & Tidy up

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  api: 'https://api.500px.com/v1/photos/search',
+  key: 'sPvXEpW2sFrch65rpyZQf01lBHuRGkEDDROTG1r4'
 };
